f31aaf120828cc9770ddc07de9adf5f68edc1c5f
oysha55 <shavkatovaoyshajon@gmail.com>
2026-04-22 19:52:34 +0500

    soat papkasi ochirildi

--- soat/main.js
+++
@@ -1,9 +0,0 @@
-const soat = document.querySelector(".soat")
-const minute = document.querySelector(".minute")
-const second = document.querySelector(".second")
-const hour = document.querySelector(".hour")
-let hours = new Data
-hours=hour
-setInterval(()=>{
-    
-})
